fd24b6e31bbc200464562591d6019b4eedcef2e3
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-16 12:46:01 +0100

    feat: initial nest app

---
+++ apps/server/src/main.ts
@@ -0,0 +1,20 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { GLOBAL_PREFIX } from './constants/globalConstants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
+  );
+}
+
+bootstrap();
