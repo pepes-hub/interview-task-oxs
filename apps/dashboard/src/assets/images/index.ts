3148951bd4bd273de6172b8744475242df2cef3a
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-20 18:46:09 +0100

    feat: introduce images

---
+++ apps/dashboard/src/assets/images/index.ts
@@ -0,0 +1,15 @@
+import example1 from './example1.png';
+import example2 from './example2.png';
+import example3 from './example3.png';
+import example4 from './example4.png';
+import example5 from './example5.png';
+import example6 from './example6.png';
+
+export const IMAGES: Record<string, string> = {
+  example1,
+  example2,
+  example3,
+  example4,
+  example5,
+  example6,
+};
