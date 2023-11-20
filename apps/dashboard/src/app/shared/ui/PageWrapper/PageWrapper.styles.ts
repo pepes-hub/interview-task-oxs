49e15cfdd04ed9c689296b616135d3e2fc36d06e
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-20 10:56:09 +0100

    feat: update PageWrapper styles

---
+++ apps/dashboard/src/app/shared/ui/PageWrapper/PageWrapper.styles.ts
@@ -0,0 +1,8 @@
+import { grey } from '@mui/material/colors';
+
+export const pageWrapperStyles = {
+  container: {
+    backgroundColor: grey[100],
+    height: '100%',
+  },
+};
---
+++ apps/dashboard/src/app/shared/ui/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import { Container, Box } from '@mui/material';
+import { pageWrapperStyles } from './PageWrapper.styles';
+
+interface PageWrapperProps {
+  children: ReactNode;
+}
+export const PageWrapper = ({ children }: PageWrapperProps) => {
+  return (
+    <Box sx={pageWrapperStyles.container}>
+      <Container maxWidth={'xl'}>{children}</Container>
+    </Box>
+  );
+};
