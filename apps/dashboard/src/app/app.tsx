76619fc6fbc88c9e72042100d1e2b360384b2a25
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-20 13:54:27 +0100

    feat: basic app composition

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,5 +1,19 @@
+import { EmployeesView } from './views/Employees';
+import { RootContainer } from './shared/ui/RootContainer';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import './libraries/i18n/I18n';
+import { theme } from './theme/muiTheme';
+
 export function App() {
-  return <div>Hello works</div>;
+  return (
+    <CssBaseline>
+      <ThemeProvider theme={theme}>
+        <RootContainer>
+          <EmployeesView />
+        </RootContainer>
+      </ThemeProvider>
+    </CssBaseline>
+  );
 }
 
 export default App;
